14831879469c426f46e1dd9d2759e8e1956bfe05
natth <natth@LAPTOP-V64916AV>
2021-12-17 11:01:03 +0700

    Add project files.

---
+++ Assignment 7 Auction/Item.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_7_Auction
+{
+    internal class Item
+    {
+        private string Descripton;
+        private double ReservePrice;
+        private double HighestBid;
+        private int BidderID;
+
+        public Item(string description, double reserveprice)
+        {
+            Descripton = description;
+            ReservePrice = reserveprice;
+            HighestBid = 0;
+            BidderID = 0;
+        }
+
+        public string GetDescription()
+        {
+            return Descripton;
+        }
+
+        public double GetReservePrice()
+        {
+            return ReservePrice;
+        }
+
+        public double GetHighestBid()
+        {
+            return HighestBid;
+        }
+
+        public double GetBidderID()
+        {
+            return BidderID;
+        }
+        public bool IsHigherBid(double BidAmount, int bidderid)
+        {
+            if (BidAmount > HighestBid)
+            {
+                HighestBid = BidAmount;
+                BidderID = bidderid;    
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsSold()
+        {
+            if (HighestBid > ReservePrice)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
---
+++ Assignment 7 Auction/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Assignment_7_Auction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Item[] items =
+            {
+                new Item("Lamp",50),
+                new Item("Teddy Bear",150),
+                new Item("Gun",250),
+                new Item("Old Coffee",550),
+            };
+
+            while (true)
+            {
+                PrintItems(items);
+                PlaceBid(items);
+                PrintSoldItems(items);
+            }
+        }
+
+        static void PrintItems(Item[] items)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine($"{i + " Description:" +  items[i].GetDescription(),-30} {"Highest Bid:" +items[i].GetHighestBid(),10}");
+            }
+        }
+
+        static void PlaceBid(Item[] items)
+        {
+            int Choice = -1;
+            while (Choice > items.Length || Choice < 0)
+            {
+                Console.WriteLine("Which item number would you like to bid on?");
+                Choice = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            
+            double BidAmount;
+            Console.WriteLine("Enter Bid amount");
+            BidAmount = Convert.ToDouble(Console.ReadLine());
+
+            int BidderID;
+            Console.WriteLine("Enter your bidder ID");
+            BidderID = Convert.ToInt32(Console.ReadLine());
+
+            items[Choice].IsHigherBid(BidAmount, BidderID);
+
+        }
+
+        static void PrintSoldItems(Item[] items)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (items[i].IsSold())
+                {
+                    Console.WriteLine(items[i].GetDescription() + " is sold  to Bidder ID:" + items[i].GetBidderID() + " for:" + items[i].GetReservePrice() + " baht" );
+                }
+            }
+        }
+    }
+}
